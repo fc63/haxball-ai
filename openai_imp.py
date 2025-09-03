5f5c820ea6be3016c19ab4a954ef6a86ebb6f635
fc63 <furkan@coban.email>
2025-09-03 11:10:16 +0300

    windows uyumlu cpu run için bazı güncellemeler

--- openai_imp.py
+++ openai_imp.py
@@ -1,4 +1,11 @@
+# -*- coding: utf-8 -*-
 import os
+# CPU tarafında bellek ve thread baskısını azalt
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # GPU aramasın, uyarılar azalsın
+
 import pickle
 import sys
 import time
@@ -6,19 +13,16 @@ from argparse import Namespace
 
 import gym
 from baselines.a2c.runner import Runner
-from baselines.common import set_global_seeds
-from baselines.common.cmd_util import make_env, make_vec_env
-from baselines.common.policies import build_policy
-from baselines.a2c.a2c import Model
 from baselines.common import set_global_seeds, explained_variance
+from baselines.common.cmd_util import make_env
+from baselines.common.policies import build_policy
 from baselines.ppo2.ppo2 import safemean
-# sys.path.append('./baselines')
 from baselines.common.vec_env import DummyVecEnv, VecEnv
 from baselines.common.vec_env.test_vec_env import SimpleEnv
-from baselines.run import build_env
+# from baselines.run import build_env  # kullanılmıyor
 
 from hx_controller.haxball_gym import Haxball
-from hx_controller.haxball_vecenv import HaxballVecEnv, HaxballSubProcVecEnv
+from hx_controller.haxball_vecenv import HaxballVecEnv  # TEK PROSES env
 from hx_controller.openai_model_torneo import A2CModel
 from simulator import create_start_conditions
 import numpy as np
@@ -35,15 +39,12 @@ if __name__ == '__main__':
     # gameplay = create_start_conditions()
     try:
         res = gym.spec('haxball-v0')
-    except:
-        gym.register(id='haxball-v0', entry_point='hx_controller.haxball_gym:Haxball', kwargs=dict(gameplay=None, max_ticks=2400))
-
-    # args_namespace = Namespace(
-    #     alg='a2c',
-    #     # env='haxball-v0',
-    #     env='PongNoFrameskip-v4',
-    #     env_type=None, gamestate=None, network=None, num_env=None, num_timesteps=1000000.0, play=False, reward_scale=1.0, save_path=None, save_video_interval=0, save_video_length=200, seed=None)
-    # env = build_env(args_namespace)
+    except Exception:
+        gym.register(
+            id='haxball-v0',
+            entry_point='hx_controller.haxball_gym:Haxball',
+            kwargs=dict(gameplay=None, max_ticks=2400)
+        )
 
     args_namespace = Namespace(
         alg='a2c',
@@ -52,13 +53,14 @@ if __name__ == '__main__':
         # env='PongNoFrameskip-v4',
         env_type=None, gamestate=None, network=None, num_timesteps=1000000.0, play=False,
         reward_scale=1.0, save_path=None, save_video_interval=0, save_video_length=200, seed=None)
-    # env2 = build_env(args_namespace)
 
     try:
         from mpi4py import MPI
     except ImportError:
         MPI = None
+
     from baselines import logger
+
     def make_vec_env(env_id, env_type, num_env, seed,
                      wrapper_kwargs=None,
                      start_index=0,
@@ -66,7 +68,7 @@ if __name__ == '__main__':
                      flatten_dict_observations=True,
                      gamestate=None):
         """
-        Create a wrapped, monitored SubprocVecEnv for Atari and MuJoCo.
+        Create a wrapped, monitored DummyVecEnv (tek proses için).
         """
         wrapper_kwargs = wrapper_kwargs or {}
         mpi_rank = MPI.COMM_WORLD.Get_rank() if MPI else 0
@@ -92,33 +94,48 @@ if __name__ == '__main__':
 
     nsteps = 5
     gamma = 0.99
-    nenvs = 100
+    nenvs = 1
     total_timesteps = int(10e7)
     log_interval = 100
-    load_path = None
+
     load_path = 'models2/ciao.h5'
     # model_i = 8
     model_i = ''
-    # load_path = 'models/%s.h5' % model_i
+
     play = bool(int(os.getenv('PLAY', False)))
-    play = False
     if play:
         nenvs = 2
 
     # env = make_vec_env(env_id='haxball-v0', env_type=None, num_env=nenvs, seed=None)
     # env = HaxballVecEnv(num_fields=nenvs, max_ticks=2400*2)
-    max_ticks = int(60*2*(1/0.1))
+    max_ticks = int(60 * 2 * (1 / 0.1))
     if play:
-        max_ticks = int(60*1*(1/0.1))
-    env = HaxballSubProcVecEnv(num_fields=nenvs, max_ticks=max_ticks)
-    # env = make_vec_env(env_id='PongNoFrameskip-v4', env_type=None, num_env=nenvs, seed=0)
-    # policy = build_policy(env=env, policy_network='lstm')#, num_layers=4, num_hidden=128)
-    policy = build_policy(env=env, policy_network='mlp', num_layers=4, num_hidden=256)
-    # policy2 = build_policy(env=env2, policy_network='mlp')
+        max_ticks = int(60 * 1 * (1 / 0.1))
 
-    # from baselines.ppo2.model import Model as PPOModel
-    # model = PPOModel(policy=policy, ob_space=env.observation_space, ac_space=env.action_space, nbatch_act=None, nsteps=nsteps, ent_coef=0.1, vf_coef=0.5, max_grad_norm=0.5, nbatch_train=None)
-    model = A2CModel(policy, model_name='p' + str(model_i) if isinstance(model_i, int) and model_i > 0 else 'a2c_model', env=env, nsteps=nsteps, ent_coef=0.1, total_timesteps=total_timesteps, lr=7e-4)# 0.005) #, vf_coef=0.0)
+    # *** KRİTİK: SubProc yerine tek-proses env ***
+    env = HaxballVecEnv(num_fields=nenvs, max_ticks=max_ticks)
+
+    # policy = build_policy(env=env, policy_network='lstm', num_layers=4, num_hidden=128)
+    policy = build_policy(env=env, policy_network='mlp', num_layers=4, num_hidden=256)
+
+    # Model
+    model = A2CModel(
+        policy,
+        model_name=('p' + str(model_i)) if isinstance(model_i, int) and model_i > 0 else 'a2c_model',
+        env=env,
+        nsteps=nsteps,
+        ent_coef=0.1,
+        total_timesteps=total_timesteps,
+        lr=7e-4  # 0.005
+        # , vf_coef=0.0
+    )
+
+    # Kayıt klasörü yoksa oluştur
+    try:
+        os.makedirs(os.path.dirname(load_path), exist_ok=True)
+    except Exception:
+        pass
+
     if load_path is not None and os.path.exists(load_path):
         model.load(load_path)
 
@@ -148,26 +165,27 @@ if __name__ == '__main__':
                 episode_rew2 = 0
                 obs = env.reset()
 
-    # Instantiate the runner object
+    # Runner
     runner = Runner(env, model, nsteps=nsteps, gamma=gamma)
     epinfobuf = deque(maxlen=100)
 
-    # Calculate the batch_size
+    # Batch
     nbatch = nenvs * nsteps
 
-    # Start total timer
+    # Timer
     tstart = time.time()
     last_rewards = []
 
-    graph_names = ('policy_entropy', 'value_loss', 'policy_loss', 'values_mean',
-                   'explained_variance',
-                   'rewards_mean', 'rewards_min', 'rewards_max', 'rewards_median', 'rewards_std',
-                   'values_mean', 'values_min', 'values_max', 'values_median', 'values_std'
-                   )
+    graph_names = (
+        'policy_entropy', 'value_loss', 'policy_loss', 'values_mean',
+        'explained_variance',
+        'rewards_mean', 'rewards_min', 'rewards_max', 'rewards_median', 'rewards_std',
+        'values_mean', 'values_min', 'values_max', 'values_median', 'values_std'
+    )
     graph_data = {k: [] for k in graph_names}
 
     for update in range(1, total_timesteps // nbatch + 1):
-        # Get mini batch of experiences
+        # Mini-batch
         obs, states, rewards, masks, actions, values, epinfos = runner.run()
 
         # invert
@@ -181,17 +199,13 @@ if __name__ == '__main__':
 
         epinfobuf.extend(epinfos)
 
-        # policy_loss, value_loss, policy_entropy = model.train(inv_obs, states, rewards, masks, inv_actions, values)
-        policy_loss, value_loss, policy_entropy = model.train(obs, states, rewards, masks, actions, values)
+        policy_loss, value_loss, policy_entropy = model.train(
+            obs, states, rewards, masks, actions, values
+        )
         nseconds = time.time() - tstart
 
-        # last_rewards += list(rewards)
-        # last_rewards = last_rewards[-20000:]
-        # Calculate the fps (frame per second)
         fps = int((update * nbatch) / nseconds)
         if update % log_interval == 0 or update == 1:
-            # Calculates if value function is a good predicator of the returns (ev > 1)
-            # or if it's just worse than predicting nothing (ev =< 0)
             ev = explained_variance(values, rewards)
             logger.record_tabular("nupdates", str(update))
             logger.record_tabular("total_timesteps", update * nbatch)
@@ -205,7 +219,7 @@ if __name__ == '__main__':
             logger.record_tabular("eplenmean", safemean([epinfo['l'] for epinfo in epinfobuf]))
             logger.dump_tabular()
 
-            # I dati per i grafici
+            # Grafik verileri
             graph_data['policy_entropy'].append(float(policy_entropy))
             graph_data['value_loss'].append(float(value_loss))
             graph_data['policy_loss'].append(float(policy_loss))
@@ -220,11 +234,15 @@ if __name__ == '__main__':
             graph_data['rewards_std'].append(np.std(rewards))
             graph_data['rewards_median'].append(np.median(rewards))
             graph_data['explained_variance'].append(float(ev))
-            with open(load_path + '.graph', 'wb') as fp:
-                pickle.dump(graph_data, fp)
+            try:
+                with open(load_path + '.graph', 'wb') as fp:
+                    pickle.dump(graph_data, fp)
+            except Exception:
+                pass
 
-            # if np.mean(rewards) > 0:
-            #     input('rewards!!!')
         if update % 5_000 == 0:
             load_path_i = load_path.replace('.h5', '_' + str(update) + '.h5')
-            model.save(load_path_i)
+            try:
+                model.save(load_path_i)
+            except Exception:
+                pass
